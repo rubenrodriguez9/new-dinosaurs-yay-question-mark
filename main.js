b3addfe5495aee7f92be7e333586214752efcad1
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-07-23 23:19:08 -0400

    finished late, gotta practice more

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-const makeDino = function() {
+const makeDino = function(species, period, carnivore, newExtinct = false) {
+// if(newExtinct === undefined){
+//   extinct = false
+// }
+
+  return {
+    species: species,
+    period: period,
+    carnivore: carnivore,
+    extinct: newExtinct
+}
 }
 
-const makeSingular = function() {
+const makeSingular = function(dino) {
+  if(dino.species.slice(-1) === 's'){
+    return makeDino(dino.species.slice(0, -2), dino.period, dino.carnivore, dino.extinct)
+  }else return dino
+ 
 }
 
-const truncateSpecies = function() {
-}
 
-const makeExtinct = function() {
 
+const truncateSpecies = function(dino) {
+  const newDino = makeDino(dino.species, dino.period, dino.carnivore, dino.extinct);
+  if(newDino.species.length > 10){
+    newDino.species = newDino.species.slice(0,7) + '...';
+    return newDino
+  }else 
+    
+  return newDino
+
+  }
+
+
+const makeExtinct = function(dinosaur) {
+  return makeDino(dinosaur.species, dinosaur.period, dinosaur.carnivore,true)
 }
 
 module.exports = {
